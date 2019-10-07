4ebd14f65f584b72a20bc2b86aadf432cd6aa1b1
puancoffee <puanngopi@gmail.com>
2019-10-07 13:25:54 +0700

    add validator for details product

--- validation/detail.js
+++ validation/detail.js
@@ -1,27 +1,22 @@
 const Validator = require("validator");
 const isEmpty = require("is-empty");
 
-module.exports = function validateCreateInput(data) {
+module.exports = function validateCreateDetail(data) {
     let errors = {};
     // Convert empty fields to an empty string so we can use validator functions
+    data.event = !isEmpty(data.event) ? data.event: "";
     data.name = !isEmpty(data.name) ? data.name: "";
-    data.description = !isEmpty(data.description) ? data.description: "";
-    data.category = !isEmpty(data.category) ? data.category: "";
     data.date = !isEmpty(data.date) ? data.date: "";
     data.time = !isEmpty(data.time) ? data.time: "";
-    data.price = !isEmpty(data.price) ? data.price: "";
+    data.location = !isEmpty(data.location) ? data.location: "";
 
+    // Event checks
+  if (Validator.isEmpty(data.event)) {
+        errors.event = "Event field is required";
+      }
     // Name checks
   if (Validator.isEmpty(data.name)) {
     errors.name = "Name field is required";
-  }
-  // Description checks
-  if (Validator.isEmpty(data.description)) {
-    errors.description = "Description field is required";
-  }
-  // Category checks
-  if (Validator.isEmpty(data.category)) {
-    errors.category = "Category field is required";
   }
   // Date checks
   if (Validator.isEmpty(data.date)) {
@@ -31,9 +26,9 @@ module.exports = function validateCreateInput(data) {
   if (Validator.isEmpty(data.time)) {
     errors.time = "Time field is required";
   }
-  // Price checks
-  if (Validator.isEmpty(data.price)) {
-    errors.price = "Price field is required";
+  // Location checks
+  if (Validator.isEmpty(data.location)) {
+    errors.location = "Location field is required";
   }
   return {
     errors,
